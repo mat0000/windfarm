9ab20cf39798b6f5e265ed04f57e31d47100adfb
Artur Matysik <art.matysik@gmail.com>
2021-07-04 23:15:42 +0800

    test

--- wind_speed/wind_speed_test.py
+++ wind_speed/wind_speed_test.py
@@ -21,7 +21,7 @@ mcp = MCP.MCP3008(spi, cs)
 # create an analog input channel on pin 0
 chan = AnalogIn(mcp, MCP.P2)
 
-wind_speed_sensor = Button("BOARD5")
+wind_speed_sensor = Button(2)
 
 wind_count = 0
 
