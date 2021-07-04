fbdb515bd8c0ffc28b9dc600322edbfdc71a15a4
Artur Matysik <art.matysik@gmail.com>
2021-07-05 00:12:43 +0800

    test

--- wind_speed/wind_speed_test.py
+++ wind_speed/wind_speed_test.py
@@ -21,22 +21,27 @@ mcp = MCP.MCP3008(spi, cs)
 # create an analog input channel on pin 0
 chan = AnalogIn(mcp, MCP.P2)
 
-wind_count = 0
-spin_prev = 0
 
+interval = 10 # measurement interval (in seconds)
+
+# make binary readout
 def spin(value):
     if(value == 65472):
         return 1
     else:
         return 0
 
-while True:
-    if(spin_prev != spin(chan.value)):
-        wind_count = wind_count + 1
-        print("spin: " + str(wind_count))
-        print("val: " + str(chan.value))
-        spin_prev = spin(chan.value)
-    else:
-        print("val: " + str(chan.value))
-    time.sleep(0.01)
+def spin_frequency(interval):
+    wind_count = 0 # spin counter
+    spin_prev = 0 # signal change 
+    while time.time() < t_end:
+        t_end = time.time() + interval
+        # detect signal change
+        if(spin_prev != spin(chan.value)):
+            wind_count = wind_count + 1
+            print("spin: " + str(wind_count))
+            spin_prev = spin(chan.value)
+    return(wind_count)
 
+wind_count = spin_frequency()
+print("spins: " + str(wind_count))
