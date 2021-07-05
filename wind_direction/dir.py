5c81b955a87f96f31c16539af9ec0174f0459c2e
Artur Matysik <art.matysik@gmail.com>
2021-07-05 21:11:11 +0800

    test

--- wind_direction/dir.py
+++ wind_direction/dir.py
@@ -20,7 +20,11 @@ mcp = MCP.MCP3008(spi, cs)
 # create an analog input channel on pin 0
 chan = AnalogIn(mcp, MCP.P3)
 
+count = 0
+volts = [2.5, 1.5, 0.3, 0.6, 0.9, 2.0, 3.0, 2.9]
 while True:
-    print("Raw ADC Value: ", chan.value)
-    print("ADC Voltage: " + str(chan.voltage) + "V")
-    time.sleep(0.1)
+    direction = round(chan.voltage, 1)
+    if not direction in volts:
+        print('Unknown value: ' + str(direction))
+    else:
+        print('Match: ' + str(direction))
