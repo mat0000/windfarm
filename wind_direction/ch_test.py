876bb371c3ed7aca8a00382fa8882310cd6499a7
Artur Matysik <art.matysik@gmail.com>
2021-07-07 10:10:16 +0800

    test

---
+++ wind_direction/ch_test.py
@@ -0,0 +1,15 @@
+import busio
+import digitalio
+import board
+import adafruit_mcp3xxx.mcp3008 as MCP
+from adafruit_mcp3xxx.analog_in import AnalogIn
+import time
+
+spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+cs = digitalio.DigitalInOut(board.D5)
+mcp = MCP.MCP3008(spi, cs)
+chan_direction = AnalogIn(mcp, MCP.P3)
+
+while True:
+    print('V: ' + str(round(chan_direction.voltage, 2)))
+    time.sleep(1)
