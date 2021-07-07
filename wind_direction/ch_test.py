eb692612fcb5f92741e2c37fafb4aa46d4bcd378
Artur Matysik <art.matysik@gmail.com>
2021-07-07 10:17:51 +0800

    test

--- wind_direction/ch_test.py
+++ wind_direction/ch_test.py
@@ -8,7 +8,7 @@ import time
 spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
 cs = digitalio.DigitalInOut(board.D5)
 mcp = MCP.MCP3008(spi, cs)
-chan_direction = AnalogIn(mcp, MCP.P3)
+chan_direction = AnalogIn(mcp, MCP.P2)
 
 while True:
     print('Voltage: ' + str(round(chan_direction.voltage, 2)))
