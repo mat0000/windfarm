e810c4b86f4fc56a6df6a8d156ee76a7109beb50
Artur Matysik <art.matysik@gmail.com>
2021-07-11 19:54:30 +0800

    test

---
+++ wind_direction/dir_test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import smbus
+import time
+
+bus = smbus.SMBus(1)
+DEVICE_ADDRESS = 0x48
+lsb = 0.012890625
+
+def direction_voltage():
+	adc = bus.read_i2c_block_data(DEVICE_ADDRESS, 0, 3)
+	return (adc[2] * lsb) ### + lsb can be added
+
+while True:
+	with open('log.txt','a') as f:
+        print(str(direction_voltage()) + "V")
+        f.write("Current readout: " + str(direction_voltage()))
+	time.sleep(1)
