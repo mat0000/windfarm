4bc8518113d9665a690793bb82ab464e67cf350f
mat0000 <mat.dolata@gmail.com>
2021-07-11 09:43:57 +0800

    update

--- wind_speed/adc_wind.py
+++ wind_speed/adc_wind.py
@@ -8,7 +8,7 @@ DEVICE_ADDRESS = 0x48
 lsb = 0.012890625
 
 def direction_voltage():
-	adc = bus.read_i2c_block_data(DEVICE_ADDRESS, 0, 3)
+	adc = bus.read_i2c_block_data(DEVICE_ADDRESS, 1, 3)
 	return (adc[2] * lsb) ### + lsb can be added
 
 while True:
