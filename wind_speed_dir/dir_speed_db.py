f7fbf96e54c88f57702135ac67a8d691e203274a
Artur Matysik <art.matysik@gmail.com>
2021-07-11 20:57:06 +0800

    test

--- wind_speed_dir/dir_speed_db.py
+++ wind_speed_dir/dir_speed_db.py
@@ -10,11 +10,6 @@ import datetime
 import math
 import statistics
 import smbus
-# import busio
-# import digitalio
-# import board
-# import adafruit_mcp3xxx.mcp3008 as MCP
-# from adafruit_mcp3xxx.analog_in import AnalogIn
 from gpiozero import Button
 
 bus = smbus.SMBus(1)
@@ -30,8 +25,8 @@ chan_speed = Button(17)
 wind_count = 0
 
 # setup 
-interval_gust = 2 # gust measurement interval (in seconds)
-interval_wind = 8 # wind measurement interval (in seconds)
+interval_gust = 20 # gust measurement interval (in seconds)
+interval_wind = 60 # wind measurement interval (in seconds)
 
 # map volt: angle 
 volts = {2.5: 0, 1.5: 45, 0.3: 90, 0.6: 135, 0.9: 180, 2.0: 225, 3.0: 270, 2.9: 315}
@@ -174,6 +169,6 @@ while True:
         print('Wind direction: ' + str(wind_direction) + ' degrees.')
 
     # insert into DB
-    # if __name__ == '__main__':
-    #     insert_speed_gust_dir(time_cur, wind_speed, gust_speed, wind_direction)
+    if __name__ == '__main__':
+        insert_speed_gust_dir(time_cur, wind_speed, gust_speed, wind_direction)
             
