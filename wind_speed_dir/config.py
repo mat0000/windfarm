d577c974a1d9f93aa094ba3c1a5af0189a016304
mat0000 <mat.dolata@gmail.com>
2021-07-25 23:08:13 +0800

    full path to pass file

--- wind_speed_dir/config.py
+++ wind_speed_dir/config.py
@@ -2,7 +2,7 @@
 from configparser import ConfigParser
 
 
-def config(filename='database.ini', section='postgresql'):
+def config(filename='/home/pi/windfarm/wind_speed_dir/database.ini', section='postgresql'):
     # create a parser
     parser = ConfigParser()
     # read config file
